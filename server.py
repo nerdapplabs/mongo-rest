8c9c871bb380977975ecfe256d6beb89cc9ce7fb
Yaashika <yaashika_mahajan@yahoo.co.in>
2018-05-11 14:48:42 +0530

    09/05/2018

--- server.py
+++ server.py
@@ -1,5 +1,17 @@
-#!/usr/bin/env python3
+from aiohttp import web
+from api import handler
+
 
 def run():
     """ Starts the Server
     """
+    app = web.Application()
+    app.router.add_get('/', handler.handler)
+    app.router.add_post('/insert', handler.new_user)
+    app.router.add_get('/find', handler.handle)
+    app.router.add_patch('/update', handler.update)
+    app.router.add_delete('/delete', handler.delete)
+    web.run_app(app)
+
+
+run()
