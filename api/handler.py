9d7d87c27e115afc383b29b64bd7440b8294676f
Yaashika <yaashika_mahajan@yahoo.co.in>
2018-05-22 15:08:49 +0530

    Syslog_changes
    Signed-off-by: Yaashika <yaashika_mahajan@yahoo.co.in>

--- api/handler.py
+++ api/handler.py
@@ -1,4 +1,4 @@
-import json
+import ujson
 
 from aiohttp import web
 
@@ -6,39 +6,55 @@ from aiohttp.web import Response
 
 from mongo import client as m_client
 
+import logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s')
+logger = logging.getLogger(__name__)
+
 
 async def add_user(request):
+    logger.info('READING THE DATA...')
     # user_id_insert_request
     user_data = await request.json()
     user_id = user_data.get("_id")
     # check if _id already exists
     check_id = await m_client.do_find_one(user_id)
     if not check_id:
+        logger.info('ADDING RECORDS...')
         inserted_data = await m_client.do_insert(user_data)
+        logger.info('INSERTED DATA...')
         # find the inserted_data
         find_data = await m_client.do_find_one(inserted_data)
-        return Response(text=json.dumps(find_data), status=200)
+        logger.debug('RECORDS: %s', find_data)
+        return Response(text=ujson.dumps(find_data), status=200)
+    logger.error('_ID ALREADY EXISTS...')
     return Response(text='_id already exists !')
 
 
 async def get_user(request):
+    logger.info('READING THE _id...')
     #  user_id_find_request
     user_id = request.match_info.get('user_id')
     # check if _id exists
     user_data = await m_client.do_find_one(user_id)
     if not user_data:
+        logging.error('INVALID _id %s !', user_id)
         return web.HTTPNotFound(text='Invalid _id !')
-    return web.Response(text=json.dumps(user_data), status=200)
+    logger.debug('RECORDS: %s', user_data)
+    return web.Response(text=ujson.dumps(user_data), status=200)
 
 
 async def delete_user(request):
+    logger.info('READING DATA...')
     # user_id_delete_request
     user_id = request.match_info.get('user_id')
     # check if _id exists
     user_data = await m_client.do_find_one(user_id)
     if not user_data:
+        logger.error('INVALID _id %s !', user_id)
         return web.HTTPNotFound(text='Invalid _id !')
     await m_client.do_del(user_data)
+    logger.info('DATA DELETED...')
     return web.Response(text='Deleted !', status=200)
 
 
@@ -46,7 +62,7 @@ async def delete_user(request):
 async def update_user(request):
     # user_id_update_request
     user_id = request.match_info.get('user_id')
-    user_data = await request.json()
+    user_data = await request.ujson()
     data = dict(user_data)
     print(data)
     doc = await m_client.do_find_one(user_id)
--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 from aiohttp import web
+
 from api import handler
 
 
 def run():
     """ Starts the Server """
+
     app = web.Application()
     app.router.add_get('/api/user/{user_id}', handler.get_user)
     app.router.add_post('/api/user', handler.add_user)
